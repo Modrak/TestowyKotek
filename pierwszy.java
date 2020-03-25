c18f6457dba3b1e870af02299b516ce66621810b
Eryczek <modrak11@.pl>
2020-03-25 21:26:15 +0100

    To jest komentarz do commita

---
+++ pierwszy.java
@@ -0,0 +1,3 @@
+To jest w pliku pierwszym
+To jest juz druga liniaa
+wiecej nie napisze
---
+++ trzeci.java
@@ -0,0 +1,4 @@
+To jest pierwsza linia w trzecim pliku.abstract
+To jest kolejna linia
+A tutaj nastepna
+to juz ostatnia linia w trzeci pliku
